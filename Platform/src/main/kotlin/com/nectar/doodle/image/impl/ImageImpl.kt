c375ef56dff321c5966fd235131c1490b1dae876
neddy <nickeddy@gmail.com>
2018-01-22 09:02:02 -0800

    - whitespace

--- Platform/src/main/kotlin/com/nectar/doodle/image/impl/ImageImpl.kt
+++ Platform/src/main/kotlin/com/nectar/doodle/image/impl/ImageImpl.kt
@@ -7,7 +7,7 @@ import org.w3c.dom.HTMLImageElement
 /**
  * Created by Nicholas Eddy on 11/17/17.
  */
-internal class ImageImpl(val image: HTMLImageElement) : Image {
+internal class ImageImpl(val image: HTMLImageElement): Image {
     override val size   = Size(image.width.toDouble(), image.height.toDouble())
     override val source = image.src
 }
