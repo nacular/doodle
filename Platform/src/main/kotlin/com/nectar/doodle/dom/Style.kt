c47f90f66c27d85f463ab57427b3a82f88b72cee
neddy <nickeddy@gmail.com>
2018-02-01 16:44:11 -0800

    - Minor tweaks - Now using <div> as block element, so updated styles

--- Platform/src/main/kotlin/com/nectar/doodle/dom/Style.kt
+++ Platform/src/main/kotlin/com/nectar/doodle/dom/Style.kt
@@ -2,6 +2,7 @@ package com.nectar.doodle.dom
 
 import com.nectar.doodle.drawing.AffineTransform
 import com.nectar.doodle.drawing.Color
+import com.nectar.doodle.drawing.Font
 import com.nectar.doodle.drawing.Font.Weight
 import com.nectar.doodle.geometry.Point
 import com.nectar.doodle.geometry.Rectangle
@@ -13,10 +14,7 @@ import kotlin.math.max
 
 private typealias Style = CSSStyleDeclaration
 
-private fun em(value: Number): String {
-    return "${value.toDouble() / 16}em" // TODO: Fixme
-}
-
+private inline fun em(value: Number) =  "${value.toDouble()}px" //"${value.toDouble() / 16}em" // TODO: Fixme
 
 internal inline fun Style.setTextIndent(value: Double) { textIndent = em(value) }
 
@@ -49,12 +47,24 @@ internal inline fun Style.setColor  (value: Color ) { color  = rgba(value) }
 internal inline fun Style.setCursor (value: Cursor) { cursor = value.toString() }
 internal inline fun Style.setOpacity(value: kotlin.Float) { opacity = value.toString() }
 
+internal fun Style.setFont(value: Font?) {
+    value?.let {
+        setFontSize(it.size)
+        setFontFamily(it.family)
+        if(it.isItalic) setFontStyle(FontStyle.Italic)
+        setFontWeight(it.weight)
+
+        return
+    }
+
+    font = "inherit"
+}
+
 internal inline fun Style.setFontStyle (value: FontStyle) { fontStyle  = value.value;     }
 internal inline fun Style.setFontWeight(value: Weight   ) { fontWeight = "${value.value}" }
 
 internal inline fun Style.setFontSize  (value: Int   ) { this.fontSize   = em(max(0, value)) }
 internal inline fun Style.setFontFamily(value: String) { this.fontFamily = value;            }
-
 
 internal inline fun Style.setDisplay (value: Display?  = null) { display  = value?.value ?: "" }
 internal inline fun Style.setPosition(value: Position? = null) { position = value?.value ?: "" }
@@ -72,11 +82,11 @@ internal inline fun Style.setBackgroundRepeat(repeat: Repeat) { backgroundRepeat
 
 internal inline fun Style.setBackgroundPosition(x: Double, y: Double) { backgroundPosition = "$x $y" }
 
-internal inline fun Style.setBorderWidth(width: Double) { borderWidth = width.toString() }
-
-internal inline fun Style.setBorderColor(color: Color? = null) { borderColor = color?.let { rgba(it) /*"#${it.hexString}"*/ } ?: "" }
-
-internal inline fun Style.setBorderStyle(style: BorderStyle) {borderStyle = style.value; }
+internal inline fun Style.setBorderWidth (value: Double           ) { borderWidth  = "${value}px" }
+internal inline fun Style.setBorderRadius(value: Double           ) { borderRadius = "${value}px" }
+internal inline fun Style.setBorderRadius(x    : Double, y: Double) { borderRadius = "${x}px / ${y}px" }
+internal inline fun Style.setBorderColor (color: Color? = null    ) { borderColor = color?.let { rgba(it) /*"#${it.hexString}"*/ } ?: "" }
+internal inline fun Style.setBorderStyle (style: BorderStyle      ) { borderStyle = style.value; }
 
 internal inline fun Style.setBorderTop   (value: Double) {borderTop    = value.toString() }
 internal inline fun Style.setBorderLeft  (value: Double) {borderLeft   = value.toString() }
@@ -120,6 +130,6 @@ internal enum class Position     (val value: String) { Absolute ("absolute"), Re
 internal enum class Overflow     (val value: String) { Hidden   ("hidden"  ), Scroll  ("scroll"  )                                                   }
 internal enum class FontStyle    (val value: String) { Normal   ("normal"  ), Italic  ("italic"  ), Oblique("oblique" )                              }
 internal enum class Visibility   (val value: String) { Hidden   ("hidden"  ), Visible ("visible" )                                                   }
-internal enum class BorderStyle  (val value: String) { Dotted   ("dotted"  ), Dashed  ("dashed"  ), Solid  ("solid"   )                              }
+internal enum class BorderStyle  (val value: String) { None     ("none"    ), Dotted   ("dotted"  ), Dashed  ("dashed"  ), Solid  ("solid"   )       }
 internal enum class TextAlignment(val value: String) { Right    ("right"   ), Center  ("center"  )                                                   }
 internal enum class VerticalAlign(val value: String) { Middle   ("middle"  )                                                                         }
--- Platform/src/main/kotlin/com/nectar/doodle/dom/SystemStyler.kt
+++ Platform/src/main/kotlin/com/nectar/doodle/dom/SystemStyler.kt
@@ -12,6 +12,7 @@ internal class SystemStylerImpl: SystemStyler {
     init {
         document.styleSheets[0].asDynamic().insertRule("body * { position:absolute;overflow:hidden }", 0)
         document.styleSheets[0].asDynamic().insertRule("body pre { overflow:visible }", 0)
+        document.styleSheets[0].asDynamic().insertRule("body div { display:inline }", 0)
 
         document.styleSheets[0].asDynamic().insertRule("pre { margin:0 }", 0)
         document.styleSheets[0].asDynamic().insertRule("svg { display:inline-block;width:100%;height:100% }", 0)
