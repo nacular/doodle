77be72c0def2d259e515398a077e89a8f24d879d
neddy <nickeddy@gmail.com>
2018-10-18 01:12:17 -0700

    - project cleanup

--- Units/src/commonMain/kotlin/com/nectar/doodle/JvmName.kt
+++
@@ -1,7 +0,0 @@
-package com.nectar.doodle
-
-/**
- * Created by Nicholas Eddy on 2/17/18.
- */
-
-expect annotation class JvmName constructor(val name: String)
--- Units/src/jvmMain/kotlin/com/nectar/doodle/JvmName.kt
+++
@@ -1,7 +0,0 @@
-package com.nectar.doodle
-
-/**
- * Created by Nicholas Eddy on 2/17/18.
- */
-
-actual typealias JvmName = kotlin.jvm.JvmName
