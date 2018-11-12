bc19988010c66638de3fa857b666d3e0de064e5a
neddy <nickeddy@gmail.com>
2018-11-11 17:22:42 -0800

    - Photo no longer tries to manage image loading

--- Controls/src/commonMain/kotlin/com/nectar/doodle/controls/Photo.kt
+++ Controls/src/commonMain/kotlin/com/nectar/doodle/controls/Photo.kt
@@ -3,30 +3,12 @@ package com.nectar.doodle.controls
 import com.nectar.doodle.core.View
 import com.nectar.doodle.drawing.Canvas
 import com.nectar.doodle.image.Image
-import com.nectar.doodle.image.ImageFactory
 
 /**
  * Created by Nicholas Eddy on 2/14/18.
  */
-class Photo private constructor(private var image: Image?): View() {
-
-    constructor(imageFactory: ImageFactory, source: String): this(null) {
-        imageFactory.load(source) {
-            image     = it
-            size      = it.size
-            idealSize = it.size
-
-            rerender()
-        }
-    }
-
+class Photo(private var image: Image): View() {
     override fun render(canvas: Canvas) {
-        image?.let {
-            canvas.image(it, bounds.atOrigin)
-        }
-    }
-
-    companion object {
-        operator fun invoke(image: Image) = Photo(image)
+        canvas.image(image, bounds.atOrigin)
     }
 }
