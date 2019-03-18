8358c5bfb311fcc58b8552ad382b588e58fef3ef
neddy <nickeddy@gmail.com>
2019-03-17 19:07:28 -0700

    - Fixed Photo so it assumes size of Image - Added new Layout

--- Controls/src/commonMain/kotlin/com/nectar/doodle/controls/Photo.kt
+++ Controls/src/commonMain/kotlin/com/nectar/doodle/controls/Photo.kt
@@ -8,6 +8,10 @@ import com.nectar.doodle.image.Image
  * Created by Nicholas Eddy on 2/14/18.
  */
 class Photo(private var image: Image): View() {
+    init {
+        size = image.size
+    }
+
     override fun render(canvas: Canvas) {
         canvas.image(image, bounds.atOrigin)
     }
---
+++ Core/src/commonMain/kotlin/com/nectar/doodle/layout/HorizontalFlowLayout.kt
@@ -0,0 +1,84 @@
+package com.nectar.doodle.layout
+
+import com.nectar.doodle.core.Layout
+import com.nectar.doodle.core.Positionable
+import com.nectar.doodle.core.View
+import com.nectar.doodle.geometry.Point
+import com.nectar.doodle.utils.HorizontalAlignment
+import com.nectar.doodle.utils.HorizontalAlignment.Center
+import com.nectar.doodle.utils.HorizontalAlignment.Left
+import com.nectar.doodle.utils.HorizontalAlignment.Right
+import kotlin.math.max
+
+/**
+ * Created by Nicholas Eddy on 3/17/19.
+ */
+class HorizontalFlowLayout(private val justification    : HorizontalAlignment = Left,
+                           private val verticalSpacing  : Double = 1.0,
+                           private val horizontalSpacing: Double = 1.0): Layout() {
+
+
+    override fun layout(positionable: Positionable) {
+        var y            = positionable.insets.top
+        var height       = 0.0
+        var spacing      = 0.0
+        val itemList     = mutableListOf<View>()
+        var lineWidth    = 0.0
+        val maxLineWidth = positionable.width - positionable.insets.left - positionable.insets.right
+
+        positionable.children.forEach { child ->
+            if (child.visible) {
+                child.idealSize?.let { child.size = it }
+
+                if (itemList.size > 1) {
+                    spacing += horizontalSpacing
+                }
+
+                val temp = lineWidth + child.width + spacing
+
+                if (temp > maxLineWidth) {
+                    when (justification) {
+                        Right  -> layoutLine(itemList,  positionable.width - lineWidth - positionable.insets.right, y)
+                        Center -> layoutLine(itemList, (positionable.width - lineWidth) / 2, y)
+                        Left   -> layoutLine(itemList, positionable.insets.left, y)
+                    }
+
+                    itemList.clear()
+
+                    lineWidth = 0.0
+                    spacing   = 0.0
+
+                    if (height > 0) {
+                        y += height + verticalSpacing
+                    }
+
+                    height = child.height
+                }
+
+                lineWidth += child.width + if (itemList.size > 0) horizontalSpacing else 0.0
+
+                itemList.add(child)
+
+                height = max(height, child.height)
+            }
+        }
+
+        if (itemList.isNotEmpty()) {
+            when (justification) {
+                Right  -> layoutLine(itemList,  positionable.width - lineWidth - positionable.insets.right, y)
+                Center -> layoutLine(itemList, (positionable.width - lineWidth) / 2, y)
+                Left   -> layoutLine(itemList, positionable.insets.left, y)
+            }
+        }
+    }
+
+    private fun layoutLine(itemList: List<View>, x: Double, y: Double) {
+        var startX = x
+
+        for (view in itemList) {
+            view.position = Point(startX, y)
+
+            startX += view.width + horizontalSpacing
+        }
+    }
+}
