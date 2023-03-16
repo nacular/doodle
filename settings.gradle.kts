40d4a04ce4c5a67a7135463031512df508c7f265
ghubstan <36207203+ghubstan@users.noreply.github.com>
2023-03-16 20:06:45 -0300

    Add Photos+Runner tutorial examples

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,28 @@
 rootProject.name = "doodle"
 
 include(
-"Animation",
-"Browser",
-"Controls",
-"Core",
-"Themes",
-"Desktop",
-"Modal",
-"Contacts",
-"ContactsRunner"
+    "Animation",
+    "Browser",
+    "Controls",
+    "Core",
+    "Themes",
+    "Desktop",
+    "Modal",
+    "Contacts",
+    "ContactsRunner",
+    "Photos",
+    "PhotosRunner"
 )
 
 project(":Animation").name = "animation"
-project(":Browser"  ).name = "browser"
-project(":Controls" ).name = "controls"
-project(":Core"     ).name = "core"
-project(":Themes"   ).name = "themes"
-project(":Desktop"  ).name = "desktop"
+project(":Browser").name = "browser"
+project(":Controls").name = "controls"
+project(":Core").name = "core"
+project(":Themes").name = "themes"
+project(":Desktop").name = "desktop"
 // Slowly add tutorial example modules
 project(":Modal").name = "modal"
 project(":Contacts").name = "contacts"
 project(":ContactsRunner").name = "contactsrunner"
+project(":Photos").name = "photos"
+project(":PhotosRunner").name = "photosrunner"
