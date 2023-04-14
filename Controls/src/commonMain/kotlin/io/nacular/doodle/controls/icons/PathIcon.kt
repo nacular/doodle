02eb93f23f9d22b95b88171eff7fa36decf65364
neddy <nickeddy@gmail.com>
2023-04-14 07:39:01 -0700

    - [API] `PathIcon` can now be created with a `Stroke` and `Paint`, making it more customizable.

--- Controls/src/commonMain/kotlin/io/nacular/doodle/controls/icons/PathIcon.kt
+++ Controls/src/commonMain/kotlin/io/nacular/doodle/controls/icons/PathIcon.kt
@@ -5,9 +5,11 @@ import io.nacular.doodle.core.View
 import io.nacular.doodle.drawing.Canvas
 import io.nacular.doodle.drawing.Color
 import io.nacular.doodle.drawing.ColorPaint
+import io.nacular.doodle.drawing.Paint
 import io.nacular.doodle.drawing.Renderer.FillRule
 import io.nacular.doodle.drawing.Renderer.FillRule.EvenOdd
 import io.nacular.doodle.drawing.Stroke
+import io.nacular.doodle.drawing.paint
 import io.nacular.doodle.geometry.Path
 import io.nacular.doodle.geometry.PathMetrics
 import io.nacular.doodle.geometry.Point
@@ -18,17 +20,22 @@ import io.nacular.doodle.geometry.Size
  */
 public open class PathIcon<in T: View>(
         private val path       : Path,
-        private val size       : Size?    = null,
-                    fill       : Color?   = null,
-                    outline    : Color?   = null,
-        private val fillRule   : FillRule = EvenOdd,
+        private val size       : Size?      = null,
+        private val fill       : Paint?     = null,
+        private val stroke     : Stroke?    = null,
+        private val fillRule   : FillRule   = EvenOdd,
                     pathMetrics: PathMetrics): Icon<T> {
+    public constructor(
+        path       : Path,
+        size       : Size?    = null,
+        fill       : Color?   = null,
+        outline    : Color?   = null,
+        fillRule   : FillRule = EvenOdd,
+        pathMetrics: PathMetrics
+    ): this(path, size, fill?.paint, outline?.let { Stroke(it) }, fillRule, pathMetrics)
 
     private val pathSize = pathMetrics.size(path)
     override fun size(view: T): Size = size ?: pathSize
-
-    private val stroke = outline?.let { Stroke    (it) }
-    private val fill   = fill?.let    { ColorPaint(it) }
 
     override fun render(view: T, canvas: Canvas, at: Point) {
         val fill = this.fill ?: view.foregroundColor?.let { ColorPaint(it) }
